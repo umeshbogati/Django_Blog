a92501872062762d214e465e28d64fca9cd243e7
umeshbogati <bogatiu17@gmail.com>
2025-06-20 16:14:52 +0545

    We have done templates works.

--- blog/views.py
+++ blog/views.py
@@ -23,11 +23,11 @@ def post_new(request):
     if request.method == "POST":
         form = PostForm(request.POST)
         if form.is_valid():
-            form.save()
-            return redirect('post_list')
-        else:
-            form = PostForm()
-            return render(request, 'blog/post_edit.html', {'form': form})
+            post=form.save()
+            return redirect('post_detail', pk=post.pk)
+    else:
+        form = PostForm()
+    return render(request, 'blog/post_edit.html', {'form': form})
 def post_edit(request, pk):
     post = get_object_or_404(post, pk=pk)
     if request.method == "POST":

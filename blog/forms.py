d1a5d6a1e8be0d43004db8f948f19ec3e78edf16
umeshbogati <bogatiu17@gmail.com>
2025-06-20 17:17:23 +0545

    Add register forms

--- blog/forms.py
+++ blog/forms.py
@@ -1,5 +1,7 @@
 from django import forms 
 from .models import Post, Comment
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.models import User
 
 class PostForm(forms.ModelForm):
     class Meta:
@@ -9,3 +11,9 @@ class CommentForm(forms.ModelForm):
     class Meta:
         model = Comment
         fields = ['author', 'content']
+
+
+class RegisterForm(UserCreationForm):
+    class Meta:
+        model=User
+        fields=['username', 'password1', 'password2']
